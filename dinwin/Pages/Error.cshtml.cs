8e8bba455df936b8396358d9bfe2768fd2415749
Einir Einisson <einir@einir.com>
2018-10-08 19:20:00 +0000

    Added testing environment (xUnit)

--- dinwin/Pages/Error.cshtml.cs
+++ dinwin/Pages/Error.cshtml.cs
@@ -13,9 +13,6 @@ namespace dinwin.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
