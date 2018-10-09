603eba795a930e469c2f64ee859f939a6e743d2f
Einir Einisson <einir@einir.com>
2018-10-09 21:50:32 +0000

    Denied!

--- dinwin/Startup.cs
+++ dinwin/Startup.cs
@@ -26,17 +26,8 @@ namespace dinwin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var builder = new ConnectionStringBuilder(Configuration["DATABASE_URL"])
-            {
-                Pooling = true,
-                TrustServerCertificate = true,
-                SslMode = SslMode.Require
-            };
-
-            services.AddEntityFrameworkNpgsql()
-                .AddDbContext<DbContext>(options => options.UseNpgsql(builder.ConnectionString));
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
