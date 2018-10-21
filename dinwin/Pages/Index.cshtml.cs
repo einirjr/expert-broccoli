3f48f82668d585678e6fc964e62378b6a86bcef4
Einir Einisson <einir@einir.com>
2018-10-21 22:47:04 +0000

    added a simple keepawake class that keeps the heroku dyno's from sleeping on the job

--- dinwin/Pages/Index.cshtml.cs
+++ dinwin/Pages/Index.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Timer = System.Timers.Timer;
 
 namespace dinwin.Pages
 {
@@ -11,7 +13,7 @@ namespace dinwin.Pages
     {
         public void OnGet()
         {
-
+            KeepAwake.Start();
         }
     }
 }
---
+++ dinwin/Pages/KeepAwake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Timer = System.Timers.Timer;
+
+namespace dinwin.Pages
+{
+    /// <summary>
+    /// Simple class that sole purpose is to keep the heroku dynos sleep deprived!
+    /// With courtesy to https://stackoverflow.com/questions/27108264/c-sharp-how-to-properly-make-a-http-web-get-request
+    /// </summary>
+    public static class KeepAwake
+    {
+        public static void Start()
+        {
+            Timer t = new Timer(1200000) {AutoReset = true};
+            t.Elapsed += t_Elapsed;
+            t.Start();
+        }
+
+        private static void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://expert-broccoli.herokuapp.com/");
+
+            var client = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            }) {BaseAddress = new Uri("https://expert-broccoli.herokuapp.com/")};
+
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
